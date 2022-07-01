701d3e6c0fb5f44c888d00261711bb37c3f62470
JeongKyungHoon <rudekpuppy@gmail.com>
2022-07-01 17:41:21 +0900

    17일차 추가

---
+++ No.017/index.js
@@ -0,0 +1,10 @@
+console.log(document.querySelector(".querySelector"));
+console.log(document.querySelectorAll(".querySelector"));
+
+const newElem = document.createElement("div");
+newElem.innerHTML = "test";
+document.querySelector(".querySelector").append(newElem);
+newElem.innerHTML += 1;
+newElem.style.height = "50px";
+newElem.style.backgroundColor = "black";
+newElem.style.color = "white";
