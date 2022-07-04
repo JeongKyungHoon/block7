9e565d330bd23a89d90775dd4e8c04754c2e8fd9
JeongKyungHoon <rudekpuppy@gmail.com>
2022-07-04 17:50:17 +0900

    마스터 초기화

--- No.014/index.js
+++
@@ -1,9 +0,0 @@
-console.log(window);
-console.log(document);
-console.log(document.documentElement);
-console.log(document.getElementsByTagName("html")[0]);
-
-console.dir(document.documentElement);
-console.dir(document.getElementsByTagName("html")[0]);
-
-console.dir(Object);
