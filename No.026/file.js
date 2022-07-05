65f2448ba51c76083e4a6c754a77cecaa9f009b3
JeongKyungHoon <rudekpuppy@gmail.com>
2022-07-05 11:41:19 +0900

    큰 파일 복사중?

--- No.026/file.js
+++ No.026/file.js
@@ -94,14 +94,14 @@ import { promises as fsProm } from "fs";
 // const writeStreamPipe = fs.createWriteStream("./pipeStream");
 // readStreamPipe.pipe(writeStreamPipe);
 
-const file = fs.createWriteStream("./big.txt");
+// const file = fs.createWriteStream("./big.txt");
 
-for (let i = 0; i <= 10000000; i++) {
-  file.write(
-    "안녕하세요. 엄청나게 큰 파일을 만들어 볼 것입니다. 각오 단단히 하세요!\n"
-  );
-}
-file.end();
+// for (let i = 0; i <= 10000000; i++) {
+//   file.write(
+//     "안녕하세요. 엄청나게 큰 파일을 만들어 볼 것입니다. 각오 단단히 하세요!\n"
+//   );
+// }
+// file.end();
 
 // 큰 파일 버퍼로 복사
 console.log("before: ", process.memoryUsage().rss);
