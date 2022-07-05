57d10a4dc8d73209d3cd1041e8651a399646c1d5
JeongKyungHoon <rudekpuppy@gmail.com>
2022-07-05 11:08:50 +0900

    큰 파일 생성 추가

--- No.026/file.js
+++ No.026/file.js
@@ -93,3 +93,13 @@ import { promises as fsProm } from "fs";
 // const readStreamPipe = fs.createReadStream("./writeStream.txt");
 // const writeStreamPipe = fs.createWriteStream("./pipeStream");
 // readStreamPipe.pipe(writeStreamPipe);
+
+const fs = require("fs");
+const file = fs.createWriteStream("./big.txt");
+
+for (let i = 0; i <= 10000000; i++) {
+  file.write(
+    "안녕하세요. 엄청나게 큰 파일을 만들어 볼 것입니다. 각오 단단히 하세요!\n"
+  );
+}
+file.end();
