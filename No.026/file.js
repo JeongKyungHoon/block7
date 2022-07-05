4faaa0a91096a955369984bcc573098a8446d1a8
JeongKyungHoon <rudekpuppy@gmail.com>
2022-07-05 12:11:39 +0900

    큰 파일 테스트 완료

--- No.026/file.js
+++ No.026/file.js
@@ -94,8 +94,8 @@ import { promises as fsProm } from "fs";
 // const writeStreamPipe = fs.createWriteStream("./pipeStream");
 // readStreamPipe.pipe(writeStreamPipe);
 
+// // 큰 파일 생성
 // const file = fs.createWriteStream("./big.txt");
-
 // for (let i = 0; i <= 10000000; i++) {
 //   file.write(
 //     "안녕하세요. 엄청나게 큰 파일을 만들어 볼 것입니다. 각오 단단히 하세요!\n"
@@ -103,17 +103,17 @@ import { promises as fsProm } from "fs";
 // }
 // file.end();
 
-// 큰 파일 버퍼로 복사
-console.log("before: ", process.memoryUsage().rss);
-const data1 = fs.readFileSync("./big.txt");
-fs.writeFileSync("./big2.txt", data1);
-console.log("buffer: ", process.memoryUsage().rss);
+// // 큰 파일 버퍼로 복사
+// console.log("before: ", process.memoryUsage().rss);
+// const data1 = fs.readFileSync("./big.txt");
+// fs.writeFileSync("./big2.txt", data1);
+// console.log("buffer: ", process.memoryUsage().rss);
 
-// 큰 파일 스트림으로 복사
-console.log("before: ", process.memoryUsage().rss);
-const readStream = fs.createReadStream("./big.txt");
-const writeStream = fs.createWriteStream("./big3.txt");
-readStream.pipe(writeStream);
-readStream.on("end", () => {
-  console.log("stream: ", process.memoryUsage().rss);
-});
+// // 큰 파일 스트림으로 복사
+// console.log("before: ", process.memoryUsage().rss);
+// const readStream = fs.createReadStream("./big.txt");
+// const writeStream = fs.createWriteStream("./big3.txt");
+// readStream.pipe(writeStream);
+// readStream.on("end", () => {
+//   console.log("stream: ", process.memoryUsage().rss);
+// });
